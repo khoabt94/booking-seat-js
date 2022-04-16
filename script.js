5896b80ecfe7273aecd52255d105a7fc52ab509f
khoabt94 <khoatienbui94@gmail.com>
2022-04-16 13:52:01 +0700

    done

--- script.js
+++ script.js
@@ -1,96 +1,51 @@
-//////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 // SELECTORS
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+const container = document.querySelector(".container");
+const seats = [...document.querySelectorAll(".row .seat:not(.occupied)")];
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
 
-//////////////////////////////////////////////////////////////
-// HELPER FUNCTIONS
+let ticketPrice = +movieSelect.value;
 
-// Show input error message
-const showError = (input, message) => {
-  const formControl = input.closest(".form-control");
-  if (!formControl) return;
-  formControl.className = "form-control error";
-  const small = formControl.querySelector("small");
-  small.textContent = message;
+///////////////////////////////////////////////////////////
+// HELPER FUNCTION
+
+// Update total and count
+const updateSelectCount = () => {
+  const selectedSeats = [...document.querySelectorAll(".row .seat.selected")];
+  const selectedSeatsCount = selectedSeats.length;
+
+  // LocalStorage
+  const seatsIndex = selectedSeats.map((seat) => seats.indexOf(seat));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  // Update UI
+  count.textContent = selectedSeatsCount;
+  total.textContent = `$${selectedSeatsCount * ticketPrice}`;
+};
+// Update UI from LocalStorage
+const updateUI = () => {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  selectedSeats.forEach((index) => seats[index].classList.add("selected"));
+  updateSelectCount();
 };
 
-// Show input success message
-const showSuccess = (input) => {
-  const formControl = input.closest(".form-control");
-  if (!formControl) return;
-  formControl.className = "form-control success";
-};
+///////////////////////////////////////////////////////////
+// EVENT LISTENERS
 
-// Check email valid
-const checkEmail = (input) => {
-  const value = input.value.trim();
-  const regex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  if (regex.test(String(value).toLowerCase())) {
-    showSuccess(input);
-  } else {
-    showError(input, "Email is not valid");
+container.addEventListener("click", (e) => {
+  const seat = e.target;
+  if (seat.classList.contains("seat") && !seat.classList.contains("occupied")) {
+    e.target.classList.toggle("selected");
+    updateSelectCount();
   }
-};
-
-const getFieldName = (input) => {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-};
-
-// Check require fields
-const checkRequired = (inputArr) => {
-  inputArr.forEach((input) => {
-    if (input.value.trim() === "") {
-      showError(input, `${getFieldName(input)} is required!`);
-    } else {
-      showSuccess(input);
-    }
-  });
-};
-
-// Check length
-const checkLength = (input, min, max) => {
-  if (input.value.trim().length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters`
-    );
-  } else if (input.value.trim().length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} must be less than ${max} characters`
-    );
-  } else {
-    showSuccess(input);
-  }
-};
-
-// Check password match
-const checkPasswordMatch = (input1, input2) => {
-  if (input2.value === "") {
-    showError(input2, `Please confirm your password`);
-    return;
-  }
-  if (input1.value !== input2.value) {
-    showError(input2, "Passwords do not match");
-  } else {
-    showSuccess(input2);
-  }
-};
-
-////////////////////////////////////////////////////////////////
-//  EVENT LISTENERS
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  checkRequired([username, email, password, password2]);
-  checkLength(username, 3, 15);
-  checkLength(password, 3, 15);
-  checkEmail(email);
-  checkPasswordMatch(password, password2);
 });
+
+// Movie Select
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectCount();
+});
+
+updateUI();
